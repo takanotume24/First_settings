47b64e1e2edb1987ae7a257969cf686140561de8
takuya <takanotume24@gmail.com>
2015-05-03 03:32:12 +0900

    first settings

---
+++ first_setteings.sh
@@ -0,0 +1,20 @@
+#echo "# First_settings" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git remote add origin https://github.com/takanotume24/First_settings.git
+#git push -u origin master
+
+#sudo code /etc/fstab
+
+sudo apt-get update
+sudo add-apt-repository ppa:dhor/myway
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get upgrade
+sudo apt-get autoremove
+sudo apt-get install gimp
+sudo apt-get install rawtherapee
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+sudo apt-get install youtube-dl
+sudo apt-get install gparted
---
+++ mcpeserver_setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get install perl
+sudo apt-get install gcc
+sudo apt-get install g++
+sudo apt-get install automake
+sudo apt-get install libtool
+sudo apt-get install autoconf
+sudo apt-get install m4
+sudo apt-get install gcc-multilib
+wget -q -O - http://get.pocketmine.net/ | bash
