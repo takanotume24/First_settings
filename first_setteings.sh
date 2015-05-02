d2641a8b228338215a20433218bac53bcf3ecc9b
takuya <takanotume24@gmail.com>
2015-05-03 03:56:39 +0900

    見やすく

--- first_setteings.sh
+++ first_setteings.sh
@@ -7,14 +7,26 @@
 
 #sudo code /etc/fstab
 
+
+sudo add-apt-repository ppa:dhor/myway -y
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y
+ 
+
+################################################
 sudo apt-get update
-sudo add-apt-repository ppa:dhor/myway
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get upgrade
-sudo apt-get autoremove
-sudo apt-get install gimp
-sudo apt-get install rawtherapee
-sudo apt-get install oracle-java8-installer
-sudo apt-get install oracle-java8-set-default
-sudo apt-get install youtube-dl
-sudo apt-get install gparted
+sudo apt-get upgrade -y
+################################################
+sudo apt-get install google-chrome-stable -y
+sudo apt-get install ubuntu-make -y
+sudo umake web visual-studio-code -y
+sudo apt-get install gimp -y
+sudo apt-get install rawtherapee -y
+sudo apt-get install oracle-java8-installer -y
+sudo apt-get install oracle-java8-set-default -y
+sudo apt-get install youtube-dl -y
+sudo apt-get install gparted -y
+################################################
+sudo apt-get autoremove -y
