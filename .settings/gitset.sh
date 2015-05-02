9407f0a692297dff07a831b772c0202f8c61e408
takuya <takanotume24@gmail.com>
2015-05-03 03:40:56 +0900

    miss

---
+++ .settings/gitset.sh
@@ -0,0 +1,6 @@
+echo "# First_settings" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/takanotume24/First_settings.git
+git push -u origin master
