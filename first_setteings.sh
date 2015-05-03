ad94ea7a5f341f398934b6ffd735d174223fb645
takanotume24 <takanotume24@gmail.com>
2015-05-03 13:05:18 +0900

    add line install

--- first_setteings.sh
+++ first_setteings.sh
@@ -13,13 +13,15 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y
- 
+sudo add-apt-repository ppa:ubuntu-wine/ppa -y
 
 ################################################
 sudo apt-get update
 sudo apt-get upgrade -y
 ################################################
+sudo apt-get install git -y
 sudo apt-get install google-chrome-stable -y
+sudo apt-get install wine -y
 sudo apt-get install ubuntu-make -y
 sudo umake web visual-studio-code -y
 sudo apt-get install gimp -y
@@ -32,6 +34,12 @@ sudo apt-get install guake
 ################################################
 sudo apt-get autoremove -y
 
+#LINE install
+google-chrome-stable http://line.me/ja/download
 
 #swapsetting
 sudo gedit /etc/fstab
+google-chrome-stable http://qiita.com/scleen_x_x/items/f3fc492bcbf0f6c2896c
+
+git clone https://github.com/takanotume24/First_settings.git
+git clone https://github.com/takanotume24/arduino.git
